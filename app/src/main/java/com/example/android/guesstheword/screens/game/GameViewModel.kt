582fb61d0b8e360fde9997d0d3b4c2ce899a94be
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2021-09-24 18:13:16 -0300

    Migrando os dados para MutableLiveData e LiveData permitindo que os dados sejam observaveis, aqui as funcionalidades do app nao se alteraram mas modificamos a tipagem dos dados

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,15 +1,16 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 
     // The current word
-    var word = ""
+    val word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    val score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     lateinit var wordList: MutableList<String>
@@ -17,6 +18,8 @@ class GameViewModel: ViewModel() {
 
     init {
         Log.i("GameViewModel","GameViewModel Created!")
+        word.value = ""
+        score.value = 0
         resetList()
         nextWord()
 
@@ -61,12 +64,13 @@ class GameViewModel: ViewModel() {
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+//        score.value =+ 1
+        score.value = (score.value)?.plus(1)
         nextWord()
     }
 
@@ -76,7 +80,7 @@ class GameViewModel: ViewModel() {
     private fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 
